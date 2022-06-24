f058b58b0b2621b9895bc30a21fa6894a4572e80
coderaman07 <coderaman07@gmail.com>
2022-06-24 18:10:19 +0530

    Updated Admin Rout

--- pages/backend.jsx
+++ pages/backend.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Router from 'next/router'
 
-export default function Backend() {
+export default function backend() {
     useEffect(() => {
         Router.push("https://still-crag-49161.herokuapp.com/admin")
     }, []);
