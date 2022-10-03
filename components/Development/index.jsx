42622f6a4b7167b527df8cb6546e3feb1d4b6c67
coderaman07 <coderaman07@gmail.com>
2022-10-03 20:56:07 +0530

    Updated the error:

--- components/Development/index.jsx
+++ components/Development/index.jsx
@@ -13,9 +13,9 @@ export default function Development({ pageHeadline }) {
                     <div className={ErrorPageStyle.centeralign}>
                         <h1>Under Maintainence</h1>
                         <h2>Website under Maintainence</h2>
-                        <p>We are very sorry for the inconvinience. This website is currently under maintainence and would be back after there's a alternative to Heroku.</p><br />
-                        <p>This website was using Heroku as Backend with Django, but due to free tier got expired by Heroku this website won't be able to show the Article by <a href="https://coderaman7.vercel.app" target={'blank'}>Aman</a></p>
-                        <br /><p>Though you still might be able to see this website's content till 2022's Hactoberfest</p>
+                        <p>We are very sorry for the inconvinience.This website is currently under maintainence and would be back after there&apos;s a alternative to Heroku.</p><br />
+                        <p>This website was using Heroku as Backend with Django, but due to free tier got expired by Heroku this website won&apos;t be able to show the Article by <a href="https://coderaman7.vercel.app" target={'blank'}>Aman</a></p>
+                        <br /><p>Though you still might be able to see this website&apos;s content till 2022&apos;s Hactoberfest</p>
                         {/* <form className="form-inline search-form mt-md-5 mt-4" action="#" method="post">
                         <input className="form-control" type="search" placeholder="Search here" aria-label="email" required />
                         <button className="btn search" type="submit">Submit</button>
